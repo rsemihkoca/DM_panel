dc9b1b5c014b81050d5956d261c807d7708d41e6
Riza Semih Koca <rsemihkoca@outlook.com>
2023-08-16 10:25:48 +0300

    commit

--- database/db.py
+++ database/db.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv(".env")
 import os
 
 SQLALCHEMY_DATABASE_URL = os.environ["POSTGRESQL_CON_STR"]
